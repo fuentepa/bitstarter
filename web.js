dce0510ce726665ef16a17727a19e60b23d8803f
fuentepa <fuentepa@gmail.com>
2013-07-10 22:12:58 +0200

    no message

--- web.js
+++ web.js
@@ -1,9 +1,17 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var buffer;
+
+fs.readFile('./index.html', function (err, data) {
+  if (err) throw err;
+  buffer = new Buffer(data);
+});
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer);
 });
 
 var port = process.env.PORT || 5000;
