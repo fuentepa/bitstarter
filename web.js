0bf4728eb1a2abe76683428e6ef299b129b17d4f
fuentepa <fuentepa@gmail.com>
2013-07-22 23:48:46 +0200

    no message

--- web.js
+++ web.js
@@ -1,20 +1,15 @@
 var express = require('express');
 var fs = require('fs');
+var htmlfile = "index.html";
 
 var app = express.createServer(express.logger());
 
-var buffer;
-
-fs.readFile('./index.html', function (err, data) {
-  if (err) throw err;
-  buffer = new Buffer(data);
-});
-
 app.get('/', function(request, response) {
-  response.send(buffer.toString('utf-8'));
+	var html = fs.readFileSync(htmlfile).toString();
+	response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+  console.log("Escuchando en " + port);
 });
